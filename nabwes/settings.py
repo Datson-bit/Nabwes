ff3a9f8eb56020df5118225bb87cbfb7800ce842
Datson-bit <datonayomide@gmail.com>
2024-12-15 11:08:34 +0100

    fourth commit

--- nabwes/settings.py
+++ nabwes/settings.py
@@ -125,6 +125,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 MEDIA_ROOT = '/media'
 MEDIA_URL = '/media/'
 
